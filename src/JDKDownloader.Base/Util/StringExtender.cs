6a8c121e5efc237275017d59fc9f2c0f694e034e
litetex <40789489+litetex@users.noreply.github.com>
2021-04-20 15:24:01 +0200

    Implemented TarGz unpacking

---
+++ src/JDKDownloader.Base/Util/StringExtender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace JDKDownloader.Base.Util
+{
+   /// <seealso cref="https://stackoverflow.com/a/187394"/>
+   public static class StringExtender
+   {
+      public static int NthIndexOf(this string target, char value, int n)
+      {
+         return NthIndexOf(target, value.ToString(), n);
+      }
+
+      public static int NthIndexOf(this string target, string value, int n)
+      {
+         Match m = Regex.Match(target, "((" + Regex.Escape(value) + ").*?){" + n + "}");
+
+         if (m.Success)
+            return m.Groups[2].Captures[n - 1].Index;
+         else
+            return -1;
+      }
+   }
+}
--- src/JDKDownloader.Base/Util/UnpackUtil.cs
+++ src/JDKDownloader.Base/Util/UnpackUtil.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.IO.Compression;
 using System.Text;
 using System.Linq;
+using System.Diagnostics.Contracts;
 
 namespace JDKDownloader.Base.Util
 {
@@ -26,18 +27,28 @@ namespace JDKDownloader.Base.Util
       /// </summary>
       /// <param name="zipArchiveName"></param>
       /// <param name="destFolder"></param>
-      public static void ExtractZIPMinDepth(string zipArchiveName, string destFolder, int mindepth = 0)
+      public static void ExtractZIPMinDepth(string zipArchiveName, string destFolder, int skipDepth = 1)
       {
+         Contract.Requires(skipDepth > 0, $"{nameof(skipDepth)} has to be greater than 0");
+
          using(var archive = ZipFile.OpenRead(zipArchiveName))
          {
             foreach (var entry in archive.Entries
                .Where(e => !string.IsNullOrEmpty(e.Name))
-               .Where(e => e.FullName.Contains('/') && e.FullName[(e.FullName.IndexOf('/') + 1)..].Length > 0)
+               .Where(e => e.FullName.Contains('/') && e.FullName[(e.FullName.NthIndexOf('/', skipDepth) + 1)..].Length > 0)
                )
             {
-               var destPath = Path.Combine(destFolder, entry.FullName[(entry.FullName.IndexOf('/') + 1)..]);
-               Directory.CreateDirectory(Path.GetDirectoryName(destPath));
-               entry.ExtractToFile(destPath);
+               string name = entry.FullName[(entry.FullName.NthIndexOf('/', skipDepth) + 1)..];
+               var destFile = Path.Combine(destFolder, name);
+
+               if (DoPathTraversalCheck(destFolder, destFile))
+               {
+                  Log.Warn($"Parent traversal in paths is not allowed! File='{name}'");
+                  continue;
+               }
+
+               Directory.CreateDirectory(Path.GetDirectoryName(destFile));
+               entry.ExtractToFile(destFile);
             } 
          }
       }
@@ -52,10 +63,11 @@ namespace JDKDownloader.Base.Util
       /// </example>
       public static void ExtractTGZ(string gzArchiveName, string destFolder)
       {
-         using (var inStream = File.OpenRead(gzArchiveName))
-         using (var gzipStream = new GZipInputStream(inStream))
-         using (var tarArchive = TarArchive.CreateInputTarArchive(gzipStream, null))
-            tarArchive.ExtractContents(destFolder);
+         using var inStream = File.OpenRead(gzArchiveName);
+         using var gzipStream = new GZipInputStream(inStream);
+         using var tarArchive = TarArchive.CreateInputTarArchive(gzipStream, null);
+
+         tarArchive.ExtractContents(destFolder);
       }
 
       /// <summary>
@@ -63,21 +75,71 @@ namespace JDKDownloader.Base.Util
       /// </summary>
       /// <param name="zipArchiveName"></param>
       /// <param name="destFolder"></param>
-      public static void ExtractTGZMinDepth(string gzArchiveName, string destFolder, int mindepth = 0)
+      /// <seealso cref="https://github.com/icsharpcode/SharpZipLib/blob/master/src/ICSharpCode.SharpZipLib/Tar/TarArchive.cs#L645"/>
+      public static void ExtractTGZMinDepth(string gzArchiveName, string destFolder, int skipDepth = 1)
       {
-         throw new NotImplementedException();
-         //using (var archive = TarArchive.CreateInputTarArchive(zipArchiveName))
-         //{
-         //   archive.
-         //   foreach (var entry in archive.Entries
-         //      .Where(e => !string.IsNullOrEmpty(e.Name))
-         //      //.Where(e => e.FullName)
-         //      )
-         //      Console.WriteLine(entry.FullName);
-         //   //entry.ExtractToFile(Path.Combine(destFolder, entry.FullName));
+         Contract.Requires(skipDepth > 0, $"{nameof(skipDepth)} has to be greater than 0");
 
+         using var inStream = File.OpenRead(gzArchiveName);
+         using var gzipStream = new GZipInputStream(inStream);
+         using var tarStream = new TarInputStream(gzipStream, null);
 
-         //}
+         TarEntry tarEntry;
+         while ((tarEntry = tarStream.GetNextEntry()) != null)
+         {
+            if (tarEntry.IsDirectory)
+               continue;
+
+            string name = tarEntry.Name;
+            if (Path.IsPathRooted(name))
+            {
+               // NOTE:
+               // for UNC names...  \\machine\share\zoom\beet.txt gives \zoom\beet.txt
+               name = name[Path.GetPathRoot(name).Length..];
+            }
+
+            // Check if we have to skip, because of the skip depth
+            if(!name.Contains('/') || name[(name.NthIndexOf('/', skipDepth) + 1)..].Length == 0)
+               continue;
+
+            // Rewrite the path according to skipDepth
+            name = name[(name.NthIndexOf('/', skipDepth) + 1)..];
+
+            // Converts the unix forward slashes in the filenames to os specific backslashes
+            name = name.Replace('/', Path.DirectorySeparatorChar);
+
+            string destFile = Path.Combine(destFolder, name);
+
+            if (DoPathTraversalCheck(destFolder, destFile))
+            {
+               Log.Warn($"Parent traversal in paths is not allowed! File='{name}'");
+               continue;
+            }
+
+            string directoryName = Path.GetDirectoryName(destFile);
+
+            // Does nothing if directory exists
+            Directory.CreateDirectory(directoryName);
+
+            using (FileStream outStr = new FileStream(destFile, FileMode.Create))
+               tarStream.CopyEntryContents(outStr);
+
+            // Set the modification date/time. This approach seems to solve timezone issues.
+            DateTime myDt = DateTime.SpecifyKind(tarEntry.ModTime, DateTimeKind.Utc);
+            File.SetLastWriteTime(destFile, myDt);
+         }
       }
+
+      /// <summary>
+      /// Checks for path traversals (e.g. abc/../../../overrideSystemBinary.exe)
+      /// </summary>
+      /// <param name="destFolder"></param>
+      /// <param name="destFile"></param>
+      /// <returns><code>true</code> when a path traversal occured</returns>
+      static bool DoPathTraversalCheck(string destFolder, string destFile)
+      {
+         return !Path.GetFullPath(destFile).StartsWith(destFolder, StringComparison.InvariantCultureIgnoreCase);
+      }
+
    }
 }
